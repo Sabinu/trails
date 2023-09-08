300fd4cb48f521cc36e7f4352ea759f3e3202c3d
Sabin Purice <sabin.purice@gmail.com>
2023-09-08 16:45:02 +0300

    working work

--- .GPT.py
+++ .GPT.py
@@ -1,84 +1,40 @@
-import pandas as pd
-import asyncio
-import aiohttp
+import json
 
-from bs4 import BeautifulSoup
-from icecream import ic
+import drawsvg as draw
 
+m = lambda i: int(i // 100)
+cx = lambda x: m(x - 120000)
+cy = lambda y: m(y - 200000)
 
-async def fetch_data(session, link):
-    async with session.get(link) as response:
-        if response.status == 200:
-            soup = BeautifulSoup(await response.text(), "html.parser")
-            return {"link": link, **parse_soup(soup)}
-        else:
-            print(f"Error: Failed to retrieve data from {link}")
-            return None
+trans = lambda c: (cx(c[0]), cy(c[1]))
 
 
-async def scrape_batch(batch_links):
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch_data(session, link) for link in batch_links]
-        results = await asyncio.gather(*tasks)
-        return [result for result in results if result is not None]
+def coordinates_to_svg_path(coordinates):
+    x, y = trans(coordinates[0])
+    path = f"M {x} {y} "
+
+    for coord in coordinates[1:]:
+        x, y = trans(coord)
+        path += f"L {x} {y} "
+
+    return path
 
 
-def parse_soup(soup):
-    title = soup.title.string.strip()
-    shortlink = soup.find("link", rel="shortlink")["href"].strip()
+with open("./data/unitati_relief.geojson") as f:
+    geojson = json.load(f)
 
-    header = soup.find("h1", class_="page-header")
-    header_text = header.text.strip() if header else None
-    header_img = header.img["src"].strip() if header and header.img else None
+d = draw.Drawing(8000, 5000, origin="center")
 
-    subheader_div = soup.find("div", class_="outer-title-route-info")
-    subheader_text = subheader_div.text.strip() if subheader_div else None
+for feat in geojson["features"]:
+    coord = feat["geometry"]["coordinates"][0]
 
-    description_div = soup.find("div", class_="pane-node-field-leadin")
-    description_text = description_div.text.strip() if description_div else None
+    d.append(
+        draw.Path(
+            coordinates_to_svg_path(coord[0]),
+            close=True,
+            fill="lightblue",
+            stroke="black",
+        )
+    )
 
-    facts_div = soup.find("div", class_="view-mode-facts")
-    if facts_div:
-        group_divs = list(facts_div.find_all("div", class_="field-group-html-element"))
-        facts_dict = {}
-
-        for div in group_divs:
-            field_item = div.find("div", class_="field-item")
-
-            if field_item:
-                key = [c for c in div.get("class") if c.startswith("group-")]
-                key = key[0].replace("group-", "")
-
-                value = field_item.text.strip()
-                facts_dict[key] = value
-    else:
-        facts_dict = None
-
-    return {
-        "title": title,
-        "short": shortlink,
-        "header": header_text,
-        "header_image": header_img,
-        "subheader": subheader_text,
-        "description": description_text,
-        **facts_dict,
-    }
-
-
-async def main(batch_size):
-    df = pd.read_csv("mm_links.csv")
-    links = df["link"][:10].tolist()
-    results = []
-
-    while links:
-        batch = links[:batch_size]
-        links = links[batch_size:]
-        batch_results = await scrape_batch(batch)
-        results.extend(batch_results)
-
-    results_df = pd.DataFrame(results)
-    results_df.to_csv("mm_details.csv", index=False)
-
-
-if __name__ == "__main__":
-    asyncio.run(main(batch_size=5))
+d.save_svg("./data/unitati_relief.svg")
